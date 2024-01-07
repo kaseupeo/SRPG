93b5012cbf5a87452237237887818e2eec416704
donghyun <donghyun3794@naver.com>
2024-01-07 18:37:14 +0900

    캐릭터 이동

--- Assets/Scripts/Utils/PathFinding.cs
+++ Assets/Scripts/Utils/PathFinding.cs
@@ -8,6 +8,7 @@ public class PathFinding
 {
     private static Dictionary<Vector2Int, Tile> _searchableTiles;
     
+    // 길찾기
     public static List<Tile> FindPath(Tile start, Tile end, List<Tile> inRangeTiles)
     {
         _searchableTiles = new Dictionary<Vector2Int, Tile>();
@@ -37,9 +38,8 @@ public class PathFinding
             if (currentTile == end)
                 return GetFinishedList(start, end);
             
-            foreach (Tile tile in GetNeighbourTiles(currentTile))
+            foreach (Tile tile in GetSurroundingTiles(currentTile.Grid2DLocation, _searchableTiles))
             {
-                // 1은 넘어갈수 있는 최대 타일 높이
                 if (closedList.Contains(tile) || IsCheckToPassTile(currentTile, tile, 1))
                     continue;
                 
@@ -58,9 +58,60 @@ public class PathFinding
         return new List<Tile>() ;
     }
 
+    // 시작 타일에서 도착 타일을 갈 수 있는지 확인하는 
     public static bool IsCheckToPassTile(Tile startTile, Tile endTile, int height = 1)
     {
         return Mathf.Abs(startTile.transform.position.z - endTile.transform.position.z) > height || endTile.IsBlocked;
+    }
+    
+    // 범위 내 타일들 찾기
+    public static List<Tile> GetTilesInRange(Vector2Int location, int range)
+    {
+        Tile startTile = Managers.Map.MapTiles[location];
+        List<Tile> inRangeTile = new List<Tile>();
+        List<Tile> tilesForPreviousStep = new List<Tile>();
+        int stepCount = 0;
+
+        inRangeTile.Add(startTile);
+        tilesForPreviousStep.Add(startTile);
+
+        while (stepCount < range)
+        {
+            List<Tile> surroundingTiles = new List<Tile>();
+            
+            foreach (Tile tile in tilesForPreviousStep)
+                surroundingTiles.AddRange(GetSurroundingTiles(new Vector2Int(tile.GridLocation.x, tile.GridLocation.y), Managers.Map.MapTiles));
+
+            inRangeTile.AddRange(surroundingTiles);
+            tilesForPreviousStep = surroundingTiles.Distinct().ToList();
+            stepCount++;
+        }
+
+        return inRangeTile.Distinct().ToList();
+    }
+    
+    // 해당 타일에서 갈 수 있는 타일 리스트
+    public static List<Tile> GetSurroundingTiles(Vector2Int originTile, Dictionary<Vector2Int, Tile> mapTiles)
+    {
+        List<Tile> surroundingTiles = new List<Tile>();
+        
+        Vector2Int tileToCheck = new Vector2Int(originTile.x + 1, originTile.y);
+        if (mapTiles.ContainsKey(tileToCheck) && !IsCheckToPassTile(mapTiles[originTile], mapTiles[tileToCheck], 1))
+            surroundingTiles.Add(mapTiles[tileToCheck]);
+
+        tileToCheck = new Vector2Int(originTile.x - 1, originTile.y);
+        if (mapTiles.ContainsKey(tileToCheck) && !IsCheckToPassTile(mapTiles[originTile], mapTiles[tileToCheck], 1))
+            surroundingTiles.Add(mapTiles[tileToCheck]);
+
+        tileToCheck = new Vector2Int(originTile.x, originTile.y + 1);
+        if (mapTiles.ContainsKey(tileToCheck) && !IsCheckToPassTile(mapTiles[originTile], mapTiles[tileToCheck], 1))
+            surroundingTiles.Add(mapTiles[tileToCheck]);
+
+        tileToCheck = new Vector2Int(originTile.x, originTile.y - 1);
+        if (mapTiles.ContainsKey(tileToCheck) && !IsCheckToPassTile(mapTiles[originTile], mapTiles[tileToCheck], 1))
+            surroundingTiles.Add(mapTiles[tileToCheck]);
+        
+        return surroundingTiles;
     }
     
     private static List<Tile> GetFinishedList(Tile start, Tile end)
@@ -79,33 +130,8 @@ public class PathFinding
         return finishedList;
     }
     
-    private static List<Tile> GetNeighbourTiles(Tile currentTile)
-    {
-        List<Tile> neighbours = new List<Tile>();
 
-        // Right
-        Vector2Int locationToCheck = new Vector2Int(currentTile.GridLocation.x + 1, currentTile.GridLocation.y);
-        if (_searchableTiles.TryGetValue(locationToCheck, out var tile))
-            neighbours.Add(tile);
-        
-        // Left
-        locationToCheck = new Vector2Int(currentTile.GridLocation.x - 1, currentTile.GridLocation.y);
-        if (_searchableTiles.TryGetValue(locationToCheck, out tile))
-            neighbours.Add(tile);
-        
-        // Top
-        locationToCheck = new Vector2Int(currentTile.GridLocation.x, currentTile.GridLocation.y + 1);
-        if (_searchableTiles.TryGetValue(locationToCheck, out tile))
-            neighbours.Add(tile);
-        
-        // Bottom
-        locationToCheck = new Vector2Int(currentTile.GridLocation.x, currentTile.GridLocation.y - 1);
-        if (_searchableTiles.TryGetValue(locationToCheck, out tile))
-            neighbours.Add(tile);
-        
-        return neighbours;
-    }
-
+    // ASTAR 방법 (맨해튼거리법)
     private static int GetManhattanDistance(Tile start, Tile tile)
     {
         return Mathf.Abs(start.GridLocation.x - tile.GridLocation.x) +
